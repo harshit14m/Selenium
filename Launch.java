efc6045c126432a402c369a61e225dc31530f78c
Harshit <harshit.14m@gmail.com>
2025-10-03 17:15:43 +0530

    second commit

--- Launch.java
+++ Launch.java
@@ -11,11 +11,12 @@ public class Launch
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
 		//driver.manage().window().minimize();
-		driver.close();
+		//driver.close();
 		String id = driver.getWindowHandle();
 		System.out.println(id);
 		String title = driver.getTitle();
 		System.out.println(title);
+		driver.quit();
 		
 	}
 
